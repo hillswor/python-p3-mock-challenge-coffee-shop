2377f0aa5e0451812921eadc7c31d8cb806af215
Bruce <bruce.hillsworth@gmail.com>
2023-05-10 12:20:56 -0600

    Passed all tests

--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -1,11 +1,22 @@
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) in range(1, 16):
+            self._name = name
+        else:
+            raise Exception
+
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
-    
-    def coffees(self, new_coffee=None):
-        from classes.coffee import Coffee
-        pass
+
+        return [o for o in Order.all if o.customer == self]
+
+    def coffees(self):
+        return list(set([o.coffee for o in self.orders()]))
